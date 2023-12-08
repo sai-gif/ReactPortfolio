efc8e587c71b62a3df54dde82eb8f50e403d124c
goutham-kaluvakolu <goutham4331@gmail.com>
2023-12-08 01:35:26 -0600

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -37,7 +37,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src={"ReactPortfolio/public/images/selfi.jpg"}
+            src={"images/selfi.jpg"}
           />
         </div>
         
